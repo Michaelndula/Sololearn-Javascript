b9e56d6c76a50ad22df6e24748d621847b1fb50c
Michael Ndula <michaelndula@gmail.com>
2022-12-09 19:40:11 +0300

    added the adding digit to screen function

--- calculator/index.js
+++ calculator/index.js
@@ -1,6 +1,17 @@
 const calculator = {
     displayValue: '0',
     firstOperand: null,
-    waitingForSecond: false,
+    waitingForSecondOperand: false,
     operator: null,
 };
+
+function inputDigit(digit){
+    const { displayValue, waitingForSecondOperand } = calculator;
+
+    if(waitingForSecondOperand === true){
+        calculator.displayValue = digit;
+        calculator.waitingForSecondOperand = false;
+    }else{
+        calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
+    }
+}
